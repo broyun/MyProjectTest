bb2d68e0babdfc75dd29e0b012ae69dde16e4952
bro <yhs4705@naver.com>
2021-10-10 13:41:09 +0900

    버튼생성

--- app/src/main/java/com/devpooh/myprojecttest/MainActivity.kt
+++ app/src/main/java/com/devpooh/myprojecttest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devpooh.myprojecttest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,10 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("SoureTree", "생성완료")
         Log.d("SoureTree", "생성완료2")
+
+        var test1 = findViewById<Button>(R.id.test1)
+        test1.setOnClickListener {
+            Log.d("SoureTree", "버튼클릭1")
+        }
     }
 }
